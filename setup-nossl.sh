9e5fcad405fc52a6b65a8f94e6d9d71982a1050a
Michael Santosti <michael@hotmail.com.br>
2023-02-02 11:35:33 -0800

    Update setup-nossl.sh
    add downloaded themes

--- setup-nossl.sh
+++ setup-nossl.sh
@@ -19,6 +19,13 @@ echo Working Directory: ${DIR_CTFD}
 #Download CTFd
 sudo git clone --single-branch https://github.com/CTFd/CTFd.git CTFd
 
+#Download Themes
+sudo git clone https://github.com/hmrserver/CTFd-theme-pixo.git CTFd/CTFd/themes/pixo
+sudo git clone https://github.com/0xdevsachin/CTFD-odin-theme.git CTFd/CTFd/themes/odin
+sudo git clone https://github.com/iver-ics/CTFd-xmas-theme.git CTFd/CTFd/themes/xmas
+sudo git clone https://github.com/chainflag/ctfd-neon-theme.git CTFd/CTFd/themes/neon
+sudo git clone https://github.com/AaronVigal/nullify-ctfd-theme.git && sudo mv ./nullify-ctfd-theme/nullify/ CTFd/CTFd/themes/ && sudo rm -rf nullify-ctfd-theme/
+
 #Install Requirements
 sudo apt-get update -y
 sudo apt-get install \
