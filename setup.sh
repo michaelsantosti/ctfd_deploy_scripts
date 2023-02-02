7abfbe3afa76009934def57871aaba258173c015
Michael Santosti <michael@hotmail.com.br>
2023-02-02 06:36:54 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,52 @@
 ##                         ##
 #############################
 
+
+
 #Set Working Directory
-DIR_NAME="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd ${DIR_NAME}
+DIR_WORK="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+#Download CTFd
+git clone --single-branch https://github.com/CTFd/CTFd.git CTFd
+
+#Install Requirements
+sudo apt-get update -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+#Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+echo ""
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker ubuntu
+
+#Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+#Install Certbot
+sudo apt-get update -y
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update -y
+sudo apt-get install certbot -y
+
+# Bind a Work Directory
+DIR_CTFD=${DIR_WORK}/CTFd
 clear
 echo 
-echo Working Directory: $DIR_NAME
+echo Working Directory: ${DIR_CTFD}
 echo 
 
 # Catching Domain Name
@@ -23,27 +63,28 @@ read domainctfd
 sudo certbot certonly -n --standalone --agree-tos -d $domainctfd 
 
 # Copy config files
-cp ./setupfiles/* .
+cp ./setupfiles/* ${DIR_CTFD}/
 
 #Create Certificates Directory
-sudo mkdir -p .data/certbot/conf/live/$domainctfd/
+sudo mkdir -p ${DIR_CTFD}/.data/certbot/conf/live/$domainctfd/
 
 #Copy Certificates
-sudo cp /etc/letsencrypt/live/$domainctfd/fullchain.pem .data/certbot/conf/live/$domainctfd/fullchain.pem
-sudo cp /etc/letsencrypt/live/$domainctfd/privkey.pem .data/certbot/conf/live/$domainctfd/privkey.pem
+sudo cp /etc/letsencrypt/live/$domainctfd/fullchain.pem ${DIR_CTFD}/.data/certbot/conf/live/$domainctfd/fullchain.pem
+sudo cp /etc/letsencrypt/live/$domainctfd/privkey.pem ${DIR_CTFD}/.data/certbot/conf/live/$domainctfd/privkey.pem
 
 #Add Domain to necessary files
-sudo sed -i 's|CTFD_DOMAIN_ADDR|'"$domainctfd"'|g' nginx.conf
-sudo sed -i 's|CTFD_DOMAIN_ADDR|'"$domainctfd"'|g' cert-renew.sh
+sudo sed -i 's|CTFD_DOMAIN_ADDR|'"$domainctfd"'|g' ${DIR_CTFD}/nginx.conf
+sudo sed -i 's|CTFD_DOMAIN_ADDR|'"$domainctfd"'|g' ${DIR_CTFD}/cert-renew.sh
 
 #Create cron to docker certificate renew
-sudo sed -i 's|WORKDIR|'"$DIR_NAME"'|g' cron_certdocker
-cp cron_certdocker /etc/cron.d/certbot  
+sudo sed -i 's|WORKDIR|'"$DIR_NAME"'|g' ${DIR_CTFD}/cron_certdocker
+cp ${DIR_CTFD}/cron_certdocker /etc/cron.d/certbot  
 
 #Default Permissions
-sudo chown -R 755 .
-sudo chown -R www-data:www-data .
-sudo chmod +x cert-renew.sh
+sudo chown -R 755 cd ${DIR_CTFD}
+sudo chown -R www-data:www-data cd ${DIR_CTFD}
+sudo chmod +x cd ${DIR_CTFD}/cert-renew.sh
 
 #Compose Docker
+cd ${DIR_CTFD}
 sudo docker-compose -f docker-compose.yml -f docker-compose-production.yml up -d
